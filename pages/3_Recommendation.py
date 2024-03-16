a988a2947d5cd624e070766b747bce0ea4f6cc7c
Shrihari Magar <shriharimagar1@gmail.com>
2024-03-16 16:56:21 +0530

    feat: modify chat prompts

--- pages/3_Recommendation.py
+++ pages/3_Recommendation.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
+st.title("Stock Guru Chatbot")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
@@ -11,54 +11,56 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-initial_message = "Welcome to our cutting-edge Conversational Recommender System for stock investments! Our goal is to provide you with personalized recommendations and real-time insights. To better assist you, could you please share the domain of companies you are interested in? Example: Technology, Healthcare"
-
-st.session_state.messages.append({"role": "assistant", "content": initial_message})
-with st.chat_message("assistant"):
-    st.markdown(initial_message)
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("Domain of companies you're interested in?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
+if len(st.session_state.messages) == 0:
+    # First message from the bot asking about the domain of companies
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "content": """Welcome to our cutting-edge Conversational Recommender System for stock investments!Our goal is to provide you with personalized recommendations and real-time insights.
+                        To better assist you, could you please share the domain of companies you are interested in ?
+                        Example: Technology, Healthcare""",
+        }
+    )
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+        st.markdown(
+            """Welcome to our cutting-edge Conversational Recommender System for stock investments!Our goal is to provide you with personalized recommendations and real-time insights.
+                To better assist you, could you please share the domain of companies you are interested in ?
+                Example: Technology, Healthcare"""
         )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+else:
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("Risk factor: high, medium, or low?"):
+if prompt := st.chat_input("You:"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+    # Second message from the bot asking about the risk factor
+    if len(st.session_state.messages) == 1:
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": "Great! Now, could you please specify the risk factor you are considering - high, medium, or low?",
+            }
         )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-if prompt := st.chat_input("Specific company name you're interested in?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+        with st.chat_message("assistant"):
+            st.markdown(
+                "Great! Now, could you please specify the risk factor you are considering - high, medium, or low?"
+            )
+    # Third message from the bot asking about a specific company
+    elif len(st.session_state.messages) == 2:
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": "Sure! Lastly, could you please provide the name of the specific company you are interested in?",
+            }
+        )
+        with st.chat_message("assistant"):
+            st.markdown(
+                "Sure! Lastly, could you please provide the name of the specific company you are interested in?"
+            )
 
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
