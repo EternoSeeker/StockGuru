5cd0e5f353ad3fed77cdb12646c9f7d49685b23a
Shrihari Magar <shriharimagar1@gmail.com>
2024-03-20 15:42:43 +0530

    feat: Analysis of stocks different periods of time

--- pages/2_Analysis.py
+++ pages/2_Analysis.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
 
 # Custom CSS for fancy design
@@ -29,51 +28,109 @@ st.markdown(
 )
 
 st.title('Real-Time Stock Performance Analysis')
-
 symbol_input = st.text_input('Enter Symbol:', key='symbol_input', placeholder='e.g AAPL')
 
-show_data = st.checkbox('Show DataFrame')
+# Add a dropdown menu for time series interval
+time_series_interval = st.selectbox('Select Time Series Interval', ['Daily', 'Weekly', 'Monthly'])
 
 if st.button('Submit', key='submit_btn'):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol_input}&apikey=5EJNPQV0T263M9MG'
-    response = requests.get(url)
+    if time_series_interval == 'Daily':
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol_input}&apikey=5EJNPQV0T263M9MG'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            time_series_data = data.get(f'Time Series ({time_series_interval})', {})
 
-    if response.status_code == 200:
-        data = response.json()
-        time_series_data = data.get('Time Series (Daily)', {})
-
-        extracted_entries = []
-        for date, entry in time_series_data.items():
-            open_price = entry.get('1. open', 'N/A')
-            high_price = entry.get('2. high', 'N/A')
-            low_price = entry.get('3. low', 'N/A')
-            close_price = entry.get('4. close', 'N/A')
-
-            extracted_entries.append({
+            extracted_entries = []
+            for date, entry in time_series_data.items():
+                open_price = entry.get('1. open', 'N/A')
+                high_price = entry.get('2. high', 'N/A')
+                low_price = entry.get('3. low', 'N/A')
+                close_price = entry.get('4. close', 'N/A')
+                extracted_entries.append({
                 'Date': date,
                 'Open': open_price,
                 'High': high_price,
                 'Low': low_price,
                 'Close': close_price
+                })
 
-            })
+            # Create DataFrame with specified column names
+            columns = ['Date', 'Close']
+            df = pd.DataFrame(extracted_entries, columns=columns)
 
-        df = pd.DataFrame(extracted_entries)
-        df['Date'] = pd.to_datetime(df['Date'])
-        df['Close'] = pd.to_numeric(df['Close'], errors='coerce')
+            # Convert 'Date' column to datetime format
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['Close'] = pd.to_numeric(df['Close'], errors='coerce')
 
-        if show_data:
             st.write("Closing Prices Over Time:")
-            st.dataframe(df)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        plt.figure(figsize=(10, 6))
-        plt.plot(df['Date'], df['Close'], marker='o', linestyle='-')
-        plt.title('Closing Prices Over Time', fontsize=18)
-        plt.xlabel('Date', fontsize=14)
-        plt.ylabel('Closing Price', fontsize=14)
-        plt.grid(True)
-        plt.xticks(rotation=45)
+            st.line_chart(df.set_index('Date')['Close'])
+            df1= pd.DataFrame(extracted_entries)
+            st.dataframe(df1)
 
-        st.pyplot()
+    elif time_series_interval == 'Weekly':
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol_input}&apikey=5EJNPQV0T263M9MG'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            time_series_data = data.get(f'Weekly Time Series', {})
+
+            extracted_entries = []
+            for date, entry in time_series_data.items():
+                open_price = entry.get('1. open', 'N/A')
+                high_price = entry.get('2. high', 'N/A')
+                low_price = entry.get('3. low', 'N/A')
+                close_price = entry.get('4. close', 'N/A')
+                extracted_entries.append({
+                'Date': date,
+                'Open': open_price,
+                'High': high_price,
+                'Low': low_price,
+                'Close': close_price
+                })
+
+            # Create DataFrame with specified column names
+            columns = ['Date', 'Close']
+            df = pd.DataFrame(extracted_entries, columns=columns)
+
+            # Convert 'Date' column to datetime format
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['Close'] = pd.to_numeric(df['Close'], errors='coerce')
+
+            st.write("Closing Prices Over Time:")
+            st.line_chart(df.set_index('Date')['Close'])
+            df1= pd.DataFrame(extracted_entries)
+            st.dataframe(df1)
+
     else:
-        st.error('Failed to fetch data')
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol_input}&apikey=5EJNPQV0T263M9MG'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            time_series_data = data.get(f'Monthly Time Series', {})
+
+            extracted_entries = []
+            for date, entry in time_series_data.items():
+                open_price = entry.get('1. open', 'N/A')
+                high_price = entry.get('2. high', 'N/A')
+                low_price = entry.get('3. low', 'N/A')
+                close_price = entry.get('4. close', 'N/A')
+                extracted_entries.append({
+                'Date': date,
+                'Open': open_price,
+                'High': high_price,
+                'Low': low_price,
+                'Close': close_price
+                })
+
+            # Create DataFrame with specified column names
+            columns = ['Date', 'Close']
+            df = pd.DataFrame(extracted_entries, columns=columns)
+            # Convert 'Date' column to datetime format
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['Close'] = pd.to_numeric(df['Close'], errors='coerce')
+
+            st.write("Closing Prices Over Time:")
+            st.line_chart(df.set_index('Date')['Close'])
+            df1= pd.DataFrame(extracted_entries)
+            st.dataframe(df1)
--- pages/7_Portfolio.py
+++ pages/7_Portfolio.py
@@ -89,36 +89,6 @@ if st.button("Save Preferences"):
 
     st.line_chart(chart_data)
 
-# url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol_input}&apikey=5EJNPQV0T263M9MG'
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         data = response.json()
-#         time_series_data = data.get('Time Series (Daily)', {})
-
-#         extracted_entries = []
-#         for date, entry in time_series_data.items():
-#             open_price = entry.get('1. open', 'N/A')
-#             high_price = entry.get('2. high', 'N/A')
-#             low_price = entry.get('3. low', 'N/A')
-#             close_price = entry.get('4. close', 'N/A')
-
-#             extracted_entries.append({
-#                 'Date': date,
-#                 'Open': open_price,
-#                 'High': high_price,
-#                 'Low': low_price,
-#                 'Close': close_price
-
-#             })
-
-#         df = pd.DataFrame(extracted_entries)
-
-# Display the user's preferences
-# user_details_container.markdown("**Preferences:**")
-# for preference, value in user_details["preferences"].items():
-#    user_details_container.markdown(f"- {preference}: {value}")
-
 # if st.button("Logout"):
 #    st.session_state["isUserLoggedIn"] = False
 #    st.session_state["userDetails"] = {}
